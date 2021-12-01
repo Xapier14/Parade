4964c045041fef672e9d9343b8459311e02a4f4e
Lance Daniel Crisang <lancecrisang14@gmail.com>
2021-12-01 18:59:14 +0800

    Removed unused LinkData class. Added DownloadThreads.

--- Downloaders/DownloadThread.cs
+++ Downloaders/DownloadThread.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Parade.Downloadables
+namespace Parade.Downloaders
 {
-    internal class LinkData
+    internal class DownloadThread
     {
-
     }
 }
