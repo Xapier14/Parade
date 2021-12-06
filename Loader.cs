f794d3a29fb0d9d6b679fe9073c2d3632ed4347e
Lance Daniel Crisang <lancecrisang14@gmail.com>
2021-12-06 09:26:20 +0800

    Loader code

---
+++ Loader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parade
+{
+    internal static class Loader
+    {
+        public static Type[] LoadDownloaders()
+        {
+            string dir = Environment.CurrentDirectory + @"\downloaders";
+            if (!Directory.Exists(dir))
+            { 
+                Directory.CreateDirectory(dir);
+            }
+            var dlls = (new DirectoryInfo(dir)).GetFiles();
+            List<Type> downloaders = new();
+            foreach (var dll in dlls)
+            {
+                if (dll.Extension == ".dll")
+                {
+                    var interf = typeof(IDownloader);
+                    var loadedFile = Assembly.LoadFile(dll.FullName);
+                    downloaders.AddRange(loadedFile.GetTypes().Where(obj =>
+                        {
+                            bool fromInterface = obj.IsAssignableFrom(interf);
+                            return interf.IsAssignableFrom(obj) && obj.IsClass && !obj.IsAbstract;
+                        }
+                    ));
+                }
+            }
+            return downloaders.ToArray();
+        }
+    }
+}
